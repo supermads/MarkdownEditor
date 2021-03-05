6314fab9cb11b833d04eb92f8323ae8585565107
supermads <madisonsfahey@gmail.com>
2021-03-05 09:31:37 -0800

    Stage 3 of Project

--- Markdown Editor/task/editor.py
+++ Markdown Editor/task/editor.py
@@ -1,14 +1,65 @@
+def get_text():
+    return input("- Text: ")
+
+
+def format_plain():
+    return get_text()
+
+
+def format_bold():
+    return f"**{get_text()}**"
+
+
+def format_italic():
+    return f"*{get_text()}*"
+
+
+def format_inline():
+    return f"`{get_text()}`"
+
+
+def format_link():
+    label = input("- Label: ")
+    url = input("- URL: ")
+    return f"[{label}]({url})"
+
+
+def format_header():
+    level = int(input("- Level: "))
+    if 1 <= level <= 6:
+        return f"{'#' * level} {get_text()}\n"
+    else:
+        return "The level should be within the range of 1 to 6"
+
+
+def format_new_line():
+    return "\n"
+
+
 def markdown_formatter():
-    formatter_options = ["plain", "bold", "italic", "link", "inline-code", "header", "ordered-list", "unordered-list", "line-break"]
+    # Ordered and unordered lists not added yet
+    formatter_funcs = {"plain": format_plain,
+                       "bold": format_bold,
+                       "italic": format_italic,
+                       "link": format_link,
+                       "inline-code": format_inline,
+                       "header":format_header,
+                       "new-line": format_new_line}
     formatter = input("- Choose a formatter: ")
+    markdown_string = ""
 
     while formatter != "!done":
         if formatter == "!help":
-            print(f"Available formatters: {' '.join(formatter_options)}")
+            print(f"Available formatters: {' '.join(formatter_funcs.keys())}")
             print("Special commands: !help !done")
 
-        elif formatter not in formatter_options:
-            print("Unknown formatting type or command. Please try again")
+        elif formatter not in formatter_funcs:
+            print("Unknown formatter or command. Please try again.")
+
+        else:
+            markdown_string += formatter_funcs[formatter]()
+
+        print(markdown_string)
 
         formatter = input("- Choose a formatter: ")
 
