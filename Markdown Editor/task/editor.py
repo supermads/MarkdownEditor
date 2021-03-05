68fe189d55f10781ac2b107aeddd4b5bf290b0af
supermads <madisonsfahey@gmail.com>
2021-03-04 19:05:43 -0800

    Stage 1 of Project

---
+++ Markdown Editor/task/editor.py
@@ -0,0 +1,8 @@
+print("# John Lennon")
+print("or ***John Winston Ono Lennon*** was one of *The Beatles*.")
+print("Here are the songs he wrote I like the most:")
+print("* Imagine")
+print("* Norwegian Wood")
+print("* Come Together")
+print("* In My Life")
+print("* ~~Hey Jude~~ (that was *McCartney*)")
